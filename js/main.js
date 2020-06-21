8bd10f19367203b499e0c5052a277b75aa6ed63d
youseffswd <60691888+youseffswd@users.noreply.github.com>
2020-06-21 12:24:21 +0300

    fifth add

--- js/main.js
+++ js/main.js
@@ -8,14 +8,228 @@ window.onmousemove = (e) => {
     }
     
 }
+let imgsCreater = document.querySelector(".portfolio .inHTML")
+let wordsC = [
+    "corporate",
+    "digital agency" ,
+    "personal blog",
+    "design bureau",
+    "showcase slider",
+    "cloud service",
+    "photo gallery",
+    "Mobile App",
+    "Showcase Slider",
+    "Conference",
+    "Showcase Slider",
+    "Classic Shop",
+    "Travel Blog",
+    "Showcase Slider",
+    "corporate",
+    "digital agency" ,
+    "personal blog",
+    "design bureau",
+    "showcase slider",
+    "cloud service",
+    "photo gallery",
+    "Mobile App",
+    "Showcase Slider",
+    "Conference",
+    "Showcase Slider"
+]
+let sentC = [
+    "agency , corporate",
+    "agency , portfolio",
+    "blog , news",
+    "agency , portfolio",
+    "agency , portfolio",
+    "service ,app , product",
+    "personal  ,  portfolio",
+    "Service, App, Product",
+    "Agency, Studio, Portfolio",
+    "Event, Conference, Meetup",
+    "Agency, Studio, Portfolio",
+    "Store, WooCommerce",
+    "Blog, News",
+    "Agency, Studio, Portfolio",
+    "agency , corporate",
+    "agency , portfolio",
+    "blog , news",
+    "agency , portfolio",
+    "agency , portfolio",
+    "service ,app , product",
+    "personal  ,  portfolio",
+    "Service, App, Product",
+    "Agency, Studio, Portfolio",
+    "Event, Conference, Meetup",
+    "Agency, Studio, Portfolio"
+]
+let y = 1;
+for(let i = 1 ; i <= 25;i++){
+    
+    imgsCreater.innerHTML += `
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="img-box cls${y}">
+                <div class="img">
+                    <img src="imgs/portfolio/DemoTemplate${i}_Home.jpg" alt="" />
+                </div>
+                <div class="info">
+                    <a href="#">${wordsC[i]}</a>
+                    <p>${sentC[i]}</p>
+                </div>
+            </div>
+        </div>
+    `
+    if(y <= 2){
+        y++
+    }else{
+        y = 1
+    }
+}
 
 
+imgsCreater.querySelectorAll(".cls1").forEach(img => img.parentElement.dataset.ani = "left")
+imgsCreater.querySelectorAll(".cls2").forEach(img => img.parentElement.dataset.ani = "down")
+imgsCreater.querySelectorAll(".cls3").forEach(img => img.parentElement.dataset.ani = "right")
 
+let imgsDiv = imgsCreater.querySelectorAll(".img img")
 
+imgsDiv.forEach(img => {
+    
+    img.onmousemove = (e) => {
+        console.log(e.layerX,e.layerY);
+        if(window.innerWidth < 576){
+            if(e.layerX > 1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${e.layerX / 14}deg)
+                                        rotateY(${-1 * (e.layerY / 8)}deg)
+                `
+            }
+            else if(e.layerX > 1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *  (e.layerX / 14)}deg)
+                                        rotateY(${e.layerY / 8}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${(e.layerX / 14)}deg)
+                                        rotateY(${-1 *(e.layerY / 8)}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *(e.layerX / 14)}deg)
+                                        rotateY(${(e.layerY / 8)}deg)
+                `
+            }
+            
+        }else if(window.innerWidth > 576 && window.innerWidth < 768){
+            if(e.layerX > 1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${e.layerX / 24}deg)
+                                        rotateY(${-1 * (e.layerY / 14)}deg)
+                `
+            }
+            else if(e.layerX > 1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *  (e.layerX / 24)}deg)
+                                        rotateY(${e.layerY / 14}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${(e.layerX / 24)}deg)
+                                        rotateY(${-1 *(e.layerY / 14)}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *(e.layerX / 24)}deg)
+                                        rotateY(${(e.layerY / 14)}deg)
+                `
+            }
 
-
-
-
+            
+        }else if(window.innerWidth > 768 && window.innerWidth < 992){
+            if(e.layerX > 1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${e.layerX / 16}deg)
+                                        rotateY(${-1 * (e.layerY / 10)}deg)
+                `
+            }
+            else if(e.layerX > 1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *  (e.layerX / 16)}deg)
+                                        rotateY(${e.layerY / 10}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${(e.layerX / 16)}deg)
+                                        rotateY(${-1 *(e.layerY / 10)}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *(e.layerX / 16)}deg)
+                                        rotateY(${(e.layerY / 10)}deg)
+                `
+            }
+        }else if(window.innerWidth > 992 && window.innerWidth < 1200){
+            if(e.layerX > 1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${e.layerX / 16}deg)
+                                        rotateY(${-1 * (e.layerY / 10)}deg)
+                `
+            }
+            else if(e.layerX > 1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *  (e.layerX / 16)}deg)
+                                        rotateY(${e.layerY / 10}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${(e.layerX / 16)}deg)
+                                        rotateY(${-1 *(e.layerY / 10)}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *(e.layerX / 16)}deg)
+                                        rotateY(${(e.layerY / 10)}deg)
+                `
+            }
+        }else{
+            if(e.layerX > 1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${e.layerX / 20}deg)
+                                        rotateY(${-1 * (e.layerY / 12)}deg)
+                `
+            }
+            else if(e.layerX > 1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *  (e.layerX / 20)}deg)
+                                        rotateY(${e.layerY / 12}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY > 1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${(e.layerX / 20)}deg)
+                                        rotateY(${-1 *(e.layerY / 12)}deg)
+                `
+            }
+            else if(e.layerX < -1 && e.layerY < -1){
+                img.style.transform = `translate(-50%,-50%) 
+                                        rotatex(${-1 *(e.layerX / 20)}deg)
+                                        rotateY(${(e.layerY / 12)}deg)
+                `
+            }
+        }
+    }
+    
+    
+})
 
 
 
@@ -80,7 +294,7 @@ window.onscroll = () => {
     
     numberssContent.forEach((number,index) => {
         if(scrollY + window.innerHeight >= number.parentElement.offsetTop + 100){
-            console.log(number.parentElement.offsetTop);
+            
             setTimeout(() => {
                 let counterr = setInterval(() => {
                     del = 0
@@ -103,7 +317,12 @@ function theStyleOfAnimation(ele,val){
 }
 
 
-// media
+setTimeout(() => {
+    
+    imgsDiv.forEach(div => {
+        div.parentElement.style.height = div.clientHeight + "px"
+    })
+}, 700);
 let height = document.querySelector(".pric2 .content .pric-badge .counter").clientHeight;
 document.querySelector(".pric2 .content .pric-badge .img").style.height = height + "px";
 document.querySelector(".pric2 .content .pric-badge .img").style.lineHeight = height + "px";
